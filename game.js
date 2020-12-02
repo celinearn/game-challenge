fe90eb752faa76f2024f3ff392a88d8ef69f106c
Celine Arnould <>
2020-12-02 14:09:01 +0100

    script bootstrap au bon endroit

---
+++ game.js
@@ -0,0 +1,12 @@
+let flyingFish = document.getElementById("flyingFish");
+const niveauDeFlotaison = 0;
+
+function main() {
+	document.getElementById("bouttonPlay").addEventListener("click", fishAuBonEndroit(){
+				return flyingFish.positionY = niveauDeFlotaison;
+		})
+	
+}
+
+main();
+fishAuBonEndroit();
